a9e689f85d82e13ceacfddac0e80fdc99ca8a593
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-05-30 09:48:56 +0400

    Add comments to collect scripts

--- tools/collect/CollectChartsMessages.bat
+++ tools/collect/CollectChartsMessages.bat
@@ -1,2 +1,10 @@
+rem Arguments:
+rem 0: <srcDir> - project root dir
+rem 1: <dstDir> - output dir
+rem 2: <language> - optional
+
+rem Example of copying English Charts messages to zz translation folder:
+rem CollectChartsMessages.bat C:\work\translations\content\en\charts C:\work\translations\content\zz
+
 @echo off
 call CollectMessages.bat %1 %2 web com.haulmont.charts.web %3
--- tools/collect/CollectFtsMessages.bat
+++ tools/collect/CollectFtsMessages.bat
@@ -1,3 +1,11 @@
+rem Arguments:
+rem 0: <srcDir> - project root dir
+rem 1: <dstDir> - output dir
+rem 2: <language> - optional
+
+rem Example of copying English FTS messages to zz translation folder:
+rem CollectFtsMessages.bat C:\work\translations\content\en\fts C:\work\translations\content\zz
+
 @echo off
 call CollectMessages.bat %1 %2 core com.haulmont.fts.core %3
 call CollectMessages.bat %1 %2 web com.haulmont.fts.web %3
